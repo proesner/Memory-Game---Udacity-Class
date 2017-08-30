093925c1edb154e9e59c61ae57d859f9c35c3821
Paul Roesner <paul@paulroesner.com>
2017-08-30 10:23:11 -0500

    Card flip function works

--- script.js
+++ script.js
@@ -22,6 +22,8 @@ var cardSymbol = ["address-book", "address-book",
 var flipped = [];
 var match = 0;
 var moves = 0;
+var standardDelay = 1000;
+var doubleDelay = 1000 * 2;
 
 var cardPairs = cardSymbol.length / 2;
 
@@ -44,52 +46,57 @@ function go() {
   match, moves = 0;
   $movesEl.html(moves);
   for (var i = 0; i < cardSymbol.length; i++) {
-    $memoryGame.append($('<li class="card white-shadow"><i class="fa fa-' + cardSymbol[i] + '"></i></li>'));
+    $memoryGame.append($('<li class="card white-shadow animated"><i class="fa fa-' + cardSymbol[i] + '"></i></li>'));
   }
 }
 
 // set star rating
 function setStarRating(moves) {
+  var diamonds = 3
   if (moves > 10 && moves < 20) {
     $ratingElItems.eq(2).remove();
+    diamonds = 2
   }
   else if (moves > 21 && moves < 28) {
     $ratingElItems.eq(1).remove();
+    diamonds = 1
   }
   else if (moves > 29) {
     $ratingElItems.eq(0).removeClass('fa fa-diamond').html('Outta diamonds!');
+    diamonds = 0
   }
+  return diamonds;
 }
-
 
 // click card behavior
 // Reference: https://stackoverflow.com/questions/9478413/not-selector-on-click-event
-$memoryGame.on('click', '.card:not(".match, .open")', function(e) {
+$memoryGame.on('click', '.card:not(".match, .open")', function() {
+  if($('.opened').length > 1) { return true; } //if card opened, no click event
   var $this = $(this);
   var card = $this.get(0).innerHTML;
 
-  $this.addClass('open show');
+  $this.addClass('open opened flipInY');
   flipped.push(card); //put i element into array, compare later
 
-  //compare with first flipped card, compare match or no match
+  //compare with first flipped card to compare match or no match
   if (flipped.length > 1) {
+    setTimeout(function() {
+      $memoryGame.find('.open').removeClass('flipInY');
+    }, standardDelay);
+
     if (card === flipped[0]) {
-      // if match, add match class, animate and
-      $memoryGame.find('.open').addClass('animated infinite bounce match');
+      // if match, add match class, animate bounce
       setTimeout(function() {
-        $memoryGame.find('.match').removeClass('open animated infinite bounce');
-      }, 1000);
-      match++; //verified match
+        $memoryGame.find('.open').addClass('match infinite bounce');
+      }, standardDelay);
+      setTimeout(function() {
+        $memoryGame.find('.match').removeClass('open opened infinite bounce');
+      }, doubleDelay);
+      match++; //verified match count up
     } else {
-      // if not a match, animate
-      $memoryGame.find('.open').addClass('animated infinite jello nomatch');
       setTimeout(function() {
-        $memoryGame.find('.open').removeClass('animated infinite jello');
-      }, 500);
-      //if not a match, hide flipped cards
-      setTimeout(function() {
-        $memoryGame.find('.open').removeClass('open nomatch animated infinite');
-      }, 1000);
+        $memoryGame.find('.open').removeClass('open opened');
+      }, standardDelay);
     }
 
       flipped = []; // reset flipped array to compare new set
@@ -97,7 +104,6 @@ $memoryGame.on('click', '.card:not(".match, .open")', function(e) {
       setStarRating(moves);
       $movesEl.html(moves);
     }
-    console.log('cardPairs= ' + cardPairs + ' match= ' + match);
 })
 
 //restart game
@@ -112,15 +118,17 @@ $restartBtn.on('click', function() {
 //endgame if all cards are flipped
 if (cardPairs === match) {
   //setRating(moves);
-  var score = score // to do, set score
+  var loot = endgame; // to do, set score
   setTimeout (function() {
-    alertify.confirm("Congrats! You've won in " + moves + " moves. Would you like to play again?", function () {
+    alertify.confirm("Congrats! You've won in " + moves + " moves, and "
+                      + loot + "left. Would you like to play again?",
+    function () {
       go();
-  }, function() {
+    }, function() {
       return;
-  })
-  }, 500);
-}
+    })
+      }, 500);
+  }
 
 //start game
 go();
